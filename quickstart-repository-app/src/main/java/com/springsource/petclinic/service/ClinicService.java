6b4520a3947c063ccaf369ff380d6ec99660e598
Paula <ilenca@hotmail.com>
2015-06-30 11:08:42 +0200

    Updated web services

---
+++ quickstart-repository-app/src/main/java/com/springsource/petclinic/service/ClinicService.java
@@ -0,0 +1,13 @@
+package com.springsource.petclinic.service;
+import org.springframework.stereotype.Service;
+import org.gvnix.service.roo.addon.annotations.GvNIXWebService;
+
+@Service
+@GvNIXWebService(name = "ws_clinicPortType", targetNamespace = "http://service.petclinic.springsource.com/", serviceName = "ws_clinic", address = "ClinicService", fullyQualifiedTypeName = "com.springsource.petclinic.service.ClinicService", exported = false)
+public class ClinicService {
+
+    public void ws_list_pets() {
+        // TODO: You have to place the method logic here.
+        return;
+    }
+}
