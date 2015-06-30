449e97ec4d10440a5a19817d7845d8652bf873b8
Paula <ilenca@hotmail.com>
2015-06-30 11:12:50 +0200

    Updated README

--- quickstart-repository-app/src/main/java/com/springsource/petclinic/service/PetService.java
+++ quickstart-repository-app/src/main/java/com/springsource/petclinic/service/PetService.java
@@ -1,5 +1,6 @@
 package com.springsource.petclinic.service;
 import org.springframework.roo.addon.layers.service.RooService;
+import org.gvnix.service.roo.addon.annotations.GvNIXWebService;
 
 @RooService(domainTypes = { com.springsource.petclinic.domain.Pet.class })
 public interface PetService {
