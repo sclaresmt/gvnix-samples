d966daab8e09e1dda7ed9bbe31bb7fe317cec0a0
pn4v4rr0 <ilenca1990@gmail.com>
2015-10-22 12:45:25 +0200

    Updated vet loupe list

---
+++ quickstart-app/src/main/java/com/springsource/petclinic/web/VetListController.java
@@ -0,0 +1,15 @@
+package com.springsource.petclinic.web;
+import com.springsource.petclinic.domain.Vet;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.gvnix.addon.web.mvc.annotations.jquery.GvNIXWebJQuery;
+import org.gvnix.addon.datatables.annotations.GvNIXDatatables;
+
+@RequestMapping("/vetlist")
+@Controller
+@RooWebScaffold(path = "vetlist", formBackingObject = Vet.class)
+@GvNIXWebJQuery
+@GvNIXDatatables(ajax = true)
+public class VetListController {
+}
